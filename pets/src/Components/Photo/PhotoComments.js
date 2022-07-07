0d9c616e1bd76caff7fb9bd4445e8d6f5df95cfb
Mariana Correa <mpcs270391@gmail.com>
2022-07-07 17:15:22 -0300

    Progresso: aula '0832 - Perdeu senha 2'

--- pets/src/Components/Photo/PhotoComments.js
+++ pets/src/Components/Photo/PhotoComments.js
@@ -15,7 +15,12 @@ const PhotoComments = (props) => {
 
     return (
         <>
-            <ul ref={commentsSection} className={styles.comments}>
+            <ul
+                ref={commentsSection}
+                className={`${styles.comments} ${
+                    props.single ? styles.single : ""
+                }`}
+            >
                 {comments.map((comment) => (
                     <li key={comment.comment_ID}>
                         <b>{comment.comment_author}: </b>
@@ -24,7 +29,11 @@ const PhotoComments = (props) => {
                 ))}
             </ul>
             {login && (
-                <PhotoCommentsForm id={props.id} setComments={setComments} />
+                <PhotoCommentsForm
+                    single={props.single}
+                    id={props.id}
+                    setComments={setComments}
+                />
             )}
         </>
     );
